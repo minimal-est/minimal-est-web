c308dafaccadf2d42d4814bea38e92e7e159c764
31n5ang <31n5ang@gmail.com>
2025-08-14 02:26:32 +0900

    🛠️refactor: 라이브러리 추가로 인한 로직 변경

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,8 +1,8 @@
+import { jwtDecode } from "jwt-decode";
+
 export const parseJwt = <T>(token: string): T | null => {
     try {
-        const base64Payload = token.split(".")[1];
-        const payload = JSON.parse(atob(base64Payload));
-        return JSON.parse(payload) as T;
+        return jwtDecode<T>(token);
     } catch(err) {
         console.error("올바르지 않은 토큰입니다.", err);
         return null;
